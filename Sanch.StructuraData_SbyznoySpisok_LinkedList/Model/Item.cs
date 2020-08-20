9a00c85bce1134d50e4c9f64656b51e9a62da6e0
Petrov Sasha <sasha1327@mail.ru>
2020-08-20 19:55:25 +0800

    Добавьте файлы проекта.

---
+++ Sanch.StructuraData_SbyznoySpisok_LinkedList/Model/Item.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.StructuraData_SbyznoySpisok_LinkedList.Model
+{
+    /// <summary>
+    /// Ячейка списка
+    /// </summary>
+    public class Item<T>
+    { 
+        /// <summary>
+        /// Данные хранимые в ячейке списка
+        /// </summary>
+        private T data = default(T);
+        public T Data
+        {
+            get
+            {
+                return data;
+            }
+
+            set
+            {
+                if (value != null)
+                {
+                    data = value;
+                }
+                else throw new ArgumentNullException(nameof(value));
+            }
+        }
+        /// <summary>
+        /// Следующая ячейка списка
+        /// </summary>
+        public Item<T> Next { get; set; }
+
+        public Item(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+
+        
+    }
+}
---
+++ Sanch.StructuraData_SbyznoySpisok_LinkedList/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.StructuraData_SbyznoySpisok_LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Описание
+            //LinkedList - DN->DN->DN (D - Data, N - Next) 1(2(3(null)))
+            //Односвязный список 1->2->3->null
+            //Двухсвязный список null<->1<->2<->3<->null
+            //Кольцевой список 1->2->3->1
+            #endregion 
+
+            var list = new Model.LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            foreach(var result in list)
+            {
+                Console.Write(result + " ");
+            }
+            Console.WriteLine();
+
+            list.Delete(3);
+            list.Delete(1);
+            list.Delete(7);
+
+            foreach (var result in list)
+            {
+                Console.Write(result + " ");
+            }
+            Console.WriteLine();
+
+            list.AppendHead(7);
+
+            foreach (var result in list)
+            {
+                Console.Write(result + " ");
+            }
+            Console.WriteLine();
+
+            list.InsertAfter(4, 8);
+
+            foreach (var result in list)
+            {
+                Console.Write(result + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
