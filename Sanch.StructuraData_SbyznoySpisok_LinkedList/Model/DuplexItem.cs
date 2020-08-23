c5665f6f4f7cb8c049c240f1b38420778e8c8c01
Petrov Sasha <sasha1327@mail.ru>
2020-08-23 22:53:56 +0800

    Добавил двухсвязанный и кольцевой список

---
+++ Sanch.StructuraData_SbyznoySpisok_LinkedList/Model/DuplexItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.StructuraData_SbyznoySpisok_LinkedList.Model
+{
+    public class DuplexItem<T>
+    {
+        public T Data { get; set; }
+        public DuplexItem<T> Previous { get; set; }
+        public DuplexItem<T> Next { get; set; }
+
+        public DuplexItem(T data)
+        {
+            //Проверка
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
--- Sanch.StructuraData_SbyznoySpisok_LinkedList/Program.cs
+++ Sanch.StructuraData_SbyznoySpisok_LinkedList/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sanch.StructuraData_SbyznoySpisok_LinkedList.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,46 +16,103 @@ namespace Sanch.StructuraData_SbyznoySpisok_LinkedList
             //Односвязный список 1->2->3->null
             //Двухсвязный список null<->1<->2<->3<->null
             //Кольцевой список 1->2->3->1
-            #endregion 
+            #endregion
 
-            var list = new Model.LinkedList<int>();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Add(4);
-            list.Add(5);
+            #region CircularLinkedList
+            var circularList = new CircularLinkedList<int>();
+            circularList.Add(1);
+            circularList.Add(2);
+            circularList.Add(3);
+            circularList.Add(4);
+            circularList.Add(5);
 
-            foreach(var result in list)
+            foreach (var result in circularList)
             {
                 Console.Write(result + " ");
             }
             Console.WriteLine();
 
-            list.Delete(3);
-            list.Delete(1);
-            list.Delete(7);
+            circularList.Delete(3);
 
-            foreach (var result in list)
+            foreach (var result in circularList)
             {
                 Console.Write(result + " ");
             }
             Console.WriteLine();
 
-            list.AppendHead(7);
+            #endregion
 
-            foreach (var result in list)
-            {
-                Console.Write(result + " ");
-            }
-            Console.WriteLine();
+            #region DuplexLinkedList
+            //var duplexList = new DuplexLinkedList<int>();
+            //duplexList.Add(1);
+            //duplexList.Add(2);
+            //duplexList.Add(3);
+            //duplexList.Add(4);
+            //duplexList.Add(5);
 
-            list.InsertAfter(4, 8);
+            //foreach(var result in duplexList)
+            //{
+            //    Console.Write(result + " ");
+            //}
+            //Console.WriteLine();
 
-            foreach (var result in list)
-            {
-                Console.Write(result + " ");
-            }
-            Console.WriteLine();
+            //duplexList.Delete(3);
+
+            //foreach (var result in duplexList)
+            //{
+            //    Console.Write(result + " ");
+            //}
+            //Console.WriteLine();
+
+            //var reverse = duplexList.Reverse();
+            //foreach (var result in reverse)
+            //{
+            //    Console.Write(result + " ");
+            //}
+            //Console.WriteLine();
+
+            #endregion
+
+            #region LinkedList
+            //var list = new Model.LinkedList<int>();
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //list.Add(4);
+            //list.Add(5);
+
+            //foreach(var result in list)
+            //{
+            //    Console.Write(result + " ");
+            //}
+            //Console.WriteLine();
+
+            //list.Delete(3);
+            //list.Delete(1);
+            //list.Delete(7);
+
+            //foreach (var result in list)
+            //{
+            //    Console.Write(result + " ");
+            //}
+            //Console.WriteLine();
+
+            //list.AppendHead(7);
+
+            //foreach (var result in list)
+            //{
+            //    Console.Write(result + " ");
+            //}
+            //Console.WriteLine();
+
+            //list.InsertAfter(4, 8);
+
+            //foreach (var result in list)
+            //{
+            //    Console.Write(result + " ");
+            //}
+            //Console.WriteLine();
+            #endregion
 
             Console.ReadLine();
         }
